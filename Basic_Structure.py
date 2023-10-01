f50188a48c7fc8ec81fafe498b90de5947a9ce6b
M Iqbal <miqbal303@gmail.com>
2023-10-01 10:30:03 +0530

    Basic_Structure

---
+++ Basic_Structure.py
@@ -0,0 +1,14 @@
+# Import the Tkinter library and alias it as 'tk'.
+import tkinter as tk
+
+# Create the main application window.
+root = tk.Tk()
+
+# Set the title of the application window.
+root.title("My first Tkinter Application")
+
+# Set the minimum size (width and height) for the application window.
+root.minsize(300, 200)
+
+# Start the Tkinter main event loop, which keeps the window open and responsive.
+root.mainloop()
